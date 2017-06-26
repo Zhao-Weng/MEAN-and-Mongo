97e57874cd5b92276badc916dc057873374fc063
zweng4 <w2280518650@gmail.com>
2017-06-25 17:36:23 -0700

    save a user with Mongo and move models and functions to separate folders to make server.js cleaner

--- back-end/server.js
+++ back-end/server.js
@@ -3,44 +3,31 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var Message = mongoose.model('Message',{
-    msg: String
-});
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 })
 
-app.get('/api/message', GetMessages)
+app.get('/api/message', message.get);
 
-app.post('/api/message', function(req,res){
-    console.log(req.body);
-    
-    var message = new Message(req.body);
-    
-    message.save();
+app.post('/api/message', message.post);
 
-    res.status(200);
-})
+app.post('/auth/register', auth.register);
 
-function GetMessages(req,res)
-{
-    Message.find({}).exec(function(err, result){
-        console.log(result);
-        res.send(result);
-    })
-}
 
-mongoose.connect("mongodb://localhost:27017/test", function(err,db){
-    if(!err){
+
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
         console.log("we are connected to mongo");
     }
 })
 
-var server = app.listen(5000, function(){
+var server = app.listen(5000, function () {
     console.log('listening on port ', server.address().port)
 })
--- front-end/src/app/index.module.js
+++ front-end/src/app/index.module.js
@@ -5,13 +5,14 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
+import { CompareToDirective } from './directives/compareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 import { MalarkeyDirective } from '../app/components/malarkey/malarkey.directive';
-import {CompareToDirective} from './directives/compareTo.directive';
 
-angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
+angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr', 'satellizer'])
+  .constant('API_URL', 'http://localhost:5000/')
   .constant('malarkey', malarkey)
   .constant('moment', moment)
   .config(config)
@@ -24,3 +25,5 @@ angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective)
   .directive('compareTo', CompareToDirective);
+
+
